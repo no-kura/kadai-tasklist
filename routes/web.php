7408b49e46498a2679743b8b12c21dbffcf7e5bb
nori.y <no.kura@kdk-nagano.co.jp>
2023-10-25 02:52:40 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TasksController::class, 'index']);
+
+Route::resource('tasks', TasksController::class);
